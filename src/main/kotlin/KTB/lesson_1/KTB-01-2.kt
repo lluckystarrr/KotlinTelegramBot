155bd8d89be31c0e3b2cd2608558612a9c72b813
Alena <luckystar@yandex-team.ru>
2026-09-01 12:53:45 +0300

    KTB-01-2 add dictionary 3

--- src/main/kotlin/KTB/lesson_1/KTB-01-2.kt
+++ src/main/kotlin/KTB/lesson_1/KTB-01-2.kt
@@ -3,11 +3,16 @@ package org.example.KTB.lesson_1
 import java.io.File
 
 fun main() {
-
     val wordsFile = File("words.txt")
-    wordsFile.createNewFile()
-    wordsFile.writeText("hello привет\n")
-    wordsFile.appendText("dog собака\n")
-    wordsFile.appendText("cat кошка\n")
-    wordsFile.readLines().forEach { println(it) }
+
+    if (!wordsFile.exists()) {
+        wordsFile.createNewFile()
+        wordsFile.writeText("hello привет\n")
+        wordsFile.appendText("dog собака\n")
+        wordsFile.appendText("cat кошка\n")
+    }
+
+    wordsFile.readLines().forEach { line ->
+        if (line.isNotBlank()) println(line)
+    }
 }
