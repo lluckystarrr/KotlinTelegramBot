6dbd057192e5dfaa25b9b069c65bc5ba44f464a4
Alena <luckystar@yandex-team.ru>
2026-09-01 13:11:25 +0300

    KTB-01-2 add dictionary 4

--- src/main/kotlin/KTB/lesson_1/KTB-01-2.kt
+++ src/main/kotlin/KTB/lesson_1/KTB-01-2.kt
@@ -1,18 +1,23 @@
 package org.example.KTB.lesson_1
 
 import java.io.File
+import java.io.IOException
 
 fun main() {
     val wordsFile = File("words.txt")
 
-    if (!wordsFile.exists()) {
-        wordsFile.createNewFile()
-        wordsFile.writeText("hello привет\n")
-        wordsFile.appendText("dog собака\n")
-        wordsFile.appendText("cat кошка\n")
-    }
+    try {
+        if (!wordsFile.exists()) {
+            wordsFile.createNewFile()
+            wordsFile.writeText("hello привет\n")
+            wordsFile.appendText("dog собака\n")
+            wordsFile.appendText("cat кошка\n")
+        }
 
-    wordsFile.readLines().forEach { line ->
-        if (line.isNotBlank()) println(line)
+        wordsFile.readLines().forEach { line ->
+            if (line.isNotBlank()) println(line)
+        }
+    } catch (e: IOException) {
+        println(e.message)
     }
 }
