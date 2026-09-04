74799cd0e86de0505e62ffb23b8d2a05d3a8cbf0
Alena <luckystar@yandex-team.ru>
2026-09-04 16:22:35 +0300

    Revert "KTB-05-1 refactoring"
    This reverts commit 92a2e82c8b42ce8095589e2ff3ed2ef8c54fd1c8.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,87 +1,16 @@
-package org.example.KTB.lesson_1
+package org.example
 
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val trainer = LearnWordsTrainer()
+    val name = "Kotlin"
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    println("Hello, " + name + "!")
 
-    while (true) {
-        println("\nМеню:")
-        println("1. Учить слова")
-        println("2. Статистика")
-        println("0. Выход")
-        print("Выберите пункт: ")
-
-        val input = readlnOrNull() ?: ""
-
-        when (input) {
-            "1" -> {
-                var question = trainer.getNextQuestion()
-
-                if (question == null) {
-                    println("Все слова в словаре выучены!")
-                    continue
-                }
-
-                while (true) {
-                    val currentQuestion = question  // сохраняем в локальную переменную
-
-                    println("\n${currentQuestion?.correctAnswer?.original}:")
-                    currentQuestion?.variants?.forEachIndexed { index, word ->
-                        println(" ${index + 1} - ${word.translate}")
-                    }
-                    println(" ----------")
-                    println(" 0 - Меню")
-                    print("Ваш выбор: ")
-
-                    val userInput = readlnOrNull()?.toIntOrNull()
-
-                    if (userInput == null) {
-                        println("Введите число от 0 до ${currentQuestion?.variants?.size}")
-                        continue
-                    }
-
-                    when (userInput) {
-                        0 -> {
-                            println("Возврат в главное меню...")
-                            break
-                        }
-                        in 1..currentQuestion?.variants?.size!! -> {
-                            val userAnswerIndex = userInput - 1
-                            val isCorrect = trainer.checkAnswer(userAnswerIndex)
-
-                            if (isCorrect) {
-                                println("Правильно!")
-                                if (currentQuestion?.correctAnswer?.isLearned()!!) {
-                                    println("Слово выучено!")
-                                }
-                            } else {
-                                println("Неправильно! ${currentQuestion?.correctAnswer?.original} – это ${currentQuestion?.correctAnswer?.translate}")
-                            }
-
-                            val nextQuestion = trainer.getNextQuestion()
-                            if (nextQuestion == null) {
-                                println("\nПоздравляем! Все слова выучены!")
-                                break
-                            }
-                            question = nextQuestion
-                            println("\nПродолжаем обучение")
-                        }
-                        else -> {
-                            println("Введите число от 0 до ${currentQuestion?.variants?.size}")
-                        }
-                    }
-                }
-            }
-            "2" -> {
-                val statistics = trainer.getStatistics()
-                println("Выучено ${statistics.learned} из ${statistics.total} слов | ${statistics.percent}%")
-            }
-            "0" -> {
-                println("До свидания!")
-                return
-            }
-            else -> {
-                println("Введите число 1, 2 или 0")
-            }
-        }
+    for (i in 1..5) {
+        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+        println("i = $i")
     }
 }
