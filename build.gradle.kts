5277a86026aeb7eddab3e72b4277bcdc004d149d
Alena <luckystar@yandex-team.ru>
2026-09-14 15:25:15 +0300

    Configure ShadowJar and Java 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "2.1.10"
     kotlin("plugin.serialization") version "2.1.10"
+    id("com.gradleup.shadow") version "8.3.6"
 }
 
 group = "org.example"
@@ -22,5 +23,5 @@ tasks.test {
 }
 
 kotlin {
-    jvmToolchain(23)
+    jvmToolchain(21)
 }
